1134d468c5cdd051dc4e89bc826649e5012fec11
David Ngozika <davidcngozika@gmail.com>
2025-07-21 13:07:09 +0100

    Display the Hero component as a child component to the HomePage component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,9 @@
-
+import Hero from '../components/Hero';
 
 export default function HomePage() {
-    return (
-        <>
-            HomePage
-        </>
-    );
+  return (
+    <>
+      <Hero />
+    </>
+  );
 }
