e5aca7745b6173565880e6f47af525ec15216dcb
David Ngozika <davidcngozika@gmail.com>
2025-07-29 19:08:52 +0100

    Limit the jobs on the HomePage to just 3

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -8,8 +8,12 @@ export default function JobListings({ isHome = false }) {
 
   useEffect(() => {
     const fetchJobs = async () => {
+      const apiUrl = isHome 
+      ? 'http://localhost:8000/jobs?_limit=3' 
+      : 'http://localhost:8000/jobs';
+
       try {
-        const res = await fetch('http://localhost:8000/jobs');
+        const res = await fetch(apiUrl);
         const data = await res.json();
         setJobs(data);
       } catch (error) {
@@ -20,7 +24,7 @@ export default function JobListings({ isHome = false }) {
     }
 
     fetchJobs();
-  }, []);
+  }, [isHome]);
 
   return (
     <section className="bg-blue-50 px-4 py-10">
